7d4d35d9551eca664cc4c9fdc3173f5a733b8239
Haobo Lv <lvhaobo@foxmail.com>
2025-07-08 15:52:04 +0800

    将数据模型迁移到 MongoDB

--- app/models.py
+++ app/models.py
@@ -1,57 +1,61 @@
-from app import db, login_manager
-from werkzeug.security import generate_password_hash, check_password_hash
+from app import mongo, login_manager
+from werkzeug.security import check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
+from bson import ObjectId
+
+class User(UserMixin):
+    def __init__(self, user_data):
+        self.user_data = user_data
+        
+    def get_id(self):
+        return str(self.user_data.get('_id'))
+        
+    @property
+    def is_authenticated(self):
+        return True
+        
+    @property
+    def is_active(self):
+        return True
+        
+    @property
+    def is_anonymous(self):
+        return False
+        
+    def check_password(self, password):
+        return check_password_hash(self.user_data.get('password_hash'), password)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    user_data = mongo.db.users.find_one({'_id': ObjectId(user_id)})
+    if not user_data:
+        return None
+    return User(user_data)
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
+def create_member(data):
+    return mongo.db.members.insert_one(data)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
+def get_member(member_id):
+    return mongo.db.members.find_one({'_id': ObjectId(member_id)})
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+def update_member(member_id, data):
+    return mongo.db.members.update_one(
+        {'_id': ObjectId(member_id)},
+        {'$set': data}
+    )
 
-    def __repr__(self):
-        return f'<User {self.username}>'
+def delete_member(member_id):
+    return mongo.db.members.delete_one({'_id': ObjectId(member_id)})
 
-class Member(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), nullable=False)
-    gender = db.Column(db.String(16))
-    id_card_number = db.Column(db.String(18), unique=True)
-    member_type = db.Column(db.String(32), nullable=False, default='学生党员')  # '学生党员' or '教师党员'
-    status = db.Column(db.String(32), nullable=False, default='积极分子') # '积极分子', '发展对象', '预备党员', '正式党员', '已归档'
-    mentors = db.Column(db.String(128))
+def create_submission(data):
+    return mongo.db.submissions.insert_one(data)
 
-    # Development dates
-    application_date = db.Column(db.Date, nullable=True)
-    activist_date = db.Column(db.Date, nullable=True)
-    development_object_date = db.Column(db.Date, nullable=True)
-    pre_member_date = db.Column(db.Date, nullable=True)
-    full_member_date = db.Column(db.Date, nullable=True)
+def get_submissions_for_member(member_id):
+    return mongo.db.submissions.find({'member_id': str(member_id)})
 
-    submissions = db.relationship('Submission', backref='member', lazy='dynamic', cascade="all, delete-orphan")
-
-    def __repr__(self):
-        return f'<Member {self.name}>'
-
-class Submission(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
-    submission_date = db.Column(db.Date, nullable=False)
-    submission_type = db.Column(db.String(64), default='思想汇报')
-
-    def __repr__(self):
-        return f'<Submission {self.id} for Member {self.member_id}>'
-
-class DismissedReminder(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    reminder_id = db.Column(db.String(128), unique=True, nullable=False)
-    dismissed_at = db.Column(db.DateTime, default=datetime.utcnow) 
+def dismiss_reminder(reminder_id):
+    return mongo.db.dismissed_reminders.insert_one({
+        'reminder_id': reminder_id,
+        'dismissed_at': datetime.utcnow()
+    }) 
